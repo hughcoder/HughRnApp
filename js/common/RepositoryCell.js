1cd30ef2b5ccf71f390544cf931557ed8b1379d8
chenyw <chenyw@tuya.com>
2018-08-13 16:26:26 +0800

    shadow

--- js/common/RepositoryCell.js
+++ js/common/RepositoryCell.js
@@ -6,18 +6,74 @@ import {
   Image,
   View,
   TextInput,
-  ListView
+  ListView,
+  TouchableOpacity
 } from "react-native";
 
 export default class RepositoryCell extends Component {
   render() {
     return (
-      <View style={{ margin: 10 }}>
-        <Text>{this.props.data.full_name}</Text>
-        <Text>{this.props.data.description}</Text>
-        <Text>{this.props.data.owner.avatar_url}</Text>
-        <Text>{this.props.data.stargazer_count}</Text>
-      </View>
+      <TouchableOpacity style={styles.container}>
+        <View style={styles.cell_container}>
+          <Text style={styles.title}>{this.props.data.full_name}</Text>
+          <Text style={styles.description}> {this.props.data.description}</Text>
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center"
+              }}
+            >
+              <Text>Author:</Text>
+              <Image
+                style={{ height: 22, width: 22 }}
+                source={{ uri: this.props.data.owner.avatar_url }}
+              />
+            </View>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <Text>Stars:</Text>
+              <Text>{this.props.data.stargazers_count}</Text>
+            </View>
+            <Image
+              style={{ width: 22, height: 22 }}
+              source={require("../../res/images/ic_star.png")}
+            />
+          </View>
+        </View>
+      </TouchableOpacity>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  title: {
+    fontSize: 16,
+    marginBottom: 2,
+    color: "#212121"
+  },
+  description: {
+    fontSize: 14,
+    marginBottom: 2,
+    color: "#757575",
+    borderRadius: 2,
+  },
+  cell_container: {
+    backgroundColor: "white",
+    padding: 10,
+    marginLeft: 5,
+    marginRight: 5,
+    marginVertical: 3,
+    borderWidth: 0.5,
+    borderRadius: 2,
+    borderColor: '#dddddd',
+    shadowColor: 'gray',
+    shadowOffset: {width:0.5,height:0.5},
+    shadowOpacity: 0.4,
+    shadowRadius: 1,
+  }
+});

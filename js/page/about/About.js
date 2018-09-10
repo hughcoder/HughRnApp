695ce2691eb7fac31043d52663e7d9337f6431c7
chenyw <chenyw@tuya.com>
2018-09-10 14:32:24 +0800

    我的页面样式

--- js/page/about/About.js
+++ js/page/about/About.js
@@ -9,109 +9,124 @@ import {
   Image,
   Dimensions,
   ListView,
+  Platform
 } from "react-native";
-import ParallaxScrollView from 'react-native-parallax-scroll-view';
-
+import ParallaxScrollView from "react-native-parallax-scroll-view";
+import ViewUtils from "../../util/ViewUtils";
+import {MORE_MENU} from '../../common/MoreMenu';
+import GlobalStyles from "../../../res/styles/GlobalStyles";
 
 export default class AboutPage extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      dataSource: new ListView.DataSource({
-        rowHasChanged: (r1, r2) => r1 !== r2
-      }).cloneWithRows([
-        "Simplicity Matters",
-        "Hammock Driven Development",
-        "Value of Values",
-        "Are We There Yet?",
-        "The Language of the System",
-        "Design, Composition, and Performance",
-        "Clojure core.async",
-        "The Functional Database",
-        "Deconstructing the Database",
-        "Hammock Driven Development",
-        "Value of Values"
-      ])
-    };
+    this.state = {};
   }
 
-  render() {
-    const { onScroll = () => {} } = this.props;
-    return (
-      <ListView
-        ref="ListView"
-        style={styles.container}
-        dataSource={this.state.dataSource}
-        renderRow={rowData => (
-          <View key={rowData} style={styles.row}>
-            <Text style={styles.rowText}>{rowData}</Text>
-          </View>
-        )}
-        renderScrollComponent={props => (
-          <ParallaxScrollView
-            onScroll={onScroll}
-            headerBackgroundColor="#333"
-            stickyHeaderHeight={STICKY_HEADER_HEIGHT}
-            parallaxHeaderHeight={PARALLAX_HEADER_HEIGHT}
-            backgroundSpeed={10}
-            renderBackground={() => (
-              <View key="background">
-                <Image
-                  source={{
-                    uri: "https://i.ytimg.com/vi/P-NZei5ANaQ/maxresdefault.jpg",
-                    width: window.width,
-                    height: PARALLAX_HEADER_HEIGHT
-                  }}
-                />
-                <View
-                  style={{
-                    position: "absolute",
-                    top: 0,
-                    width: window.width,
-                    backgroundColor: "rgba(0,0,0,.4)",
-                    height: PARALLAX_HEADER_HEIGHT
-                  }}
-                />
-              </View>
-            )}
-            renderForeground={() => (
-              <View key="parallax-header" style={styles.parallaxHeader}>
-                <Image
-                  style={styles.avatar}
-                  source={{
-                    uri:
-                      "https://pbs.twimg.com/profile_images/2694242404/5b0619220a92d391534b0cd89bf5adc1_400x400.jpeg",
-                    width: AVATAR_SIZE,
-                    height: AVATAR_SIZE
-                  }}
-                />
-                <Text style={styles.sectionSpeakerText}>
-                  Talks by Rich Hickey
-                </Text>
-                <Text style={styles.sectionTitleText}>
-                  CTO of Cognitec, Creator of Clojure
-                </Text>
-              </View>
-            )}
-            renderStickyHeader={() => (
-              <View key="sticky-header" style={styles.stickySection}>
-                <Text style={styles.stickySectionText}>Rich Hickey Talks</Text>
-              </View>
-            )}
-            renderFixedHeader={() => (
-              <View key="fixed-header" style={styles.fixedSection}>
-                <Text
-                  style={styles.fixedSectionText}
-                  onPress={() => this.refs.ListView.scrollTo({ x: 0, y: 0 })}
-                >
-                  Scroll to top
-                </Text>
-              </View>
-            )}
-          />
-        )}
-      />
+  onClick(tab) {
+    let TargetComponent,
+      isRemoveKey,
+      flag = { ...this.props, menuType: tab };
+    switch (tab) {
+      case MORE_MENU.About_Author:
+
+        break;
+      case MORE_MENU.Website:
+
+        break;
+      case MORE_MENU.Feedback:
+
+        break;
+
+    }
+    if (TargetComponent != null) {
+      this.props.navigation.navigate(TargetComponent, {
+        flag: flag,
+        isRemoveKey: isRemoveKey
+      });
+    }
+  }
+
+  getParallaxRenderConfig(params) {
+    let config = {};
+    config.renderBackground = () => (
+      <View key="background">
+        <Image
+          source={{
+            uri: params.backgroundImg,
+            width: window.width,
+            height: PARALLAX_HEADER_HEIGHT
+          }}
+        />
+        <View
+          style={{
+            position: "absolute",
+            top: 0,
+            width: window.width,
+            backgroundColor: "rgba(0,0,0,.4)",
+            height: PARALLAX_HEADER_HEIGHT
+          }}
+        />
+      </View>
     );
+    config.renderForeground = () => (
+      <View key="parallax-header" style={styles.parallaxHeader}>
+        <Image
+          style={styles.avatar}
+          source={{
+            uri: params.avatar,
+            width: AVATAR_SIZE,
+            height: AVATAR_SIZE
+          }}
+        />
+        <Text style={styles.sectionSpeakerText}>{params.name}</Text>
+        <Text style={styles.sectionTitleText}>{params.description}</Text>
+      </View>
+    );
+    config.renderStickyHeader = () => (
+      <View key="sticky-header" style={styles.stickySection}>
+        <Text style={styles.stickySectionText}>{params.name}</Text>
+      </View>
+    );
+    config.renderFixedHeader = () => (
+      <View key="fixed-header" style={styles.fixedSection}>
+        {ViewUtils.getLeftButton(() => this.props.navigation.pop())}
+      </View>
+    );
+    return config;
+  }
+
+  renderView(contentView,params) {
+    let renderConfig = this.getParallaxRenderConfig(params);
+    return (
+      <ParallaxScrollView
+        headerBackgroundColor="#333"
+        backgroundColor="#2196F3"
+        stickyHeaderHeight={STICKY_HEADER_HEIGHT}
+        parallaxHeaderHeight={PARALLAX_HEADER_HEIGHT}
+        backgroundSpeed={10}
+        {...renderConfig}
+      >
+      {contentView}
+      </ParallaxScrollView>
+    );
+  }
+  render() {
+    let content=<View style={{flexDirection:'column'}}>
+   {ViewUtils.getSettingItem(()=>this.onClick(MORE_MENU.About_Author),require('../../../res/images/ic_computer.png'),MORE_MENU.Website,{tintColor:'#2196F3'})}
+   <View style={GlobalStyles.line} />
+   {ViewUtils.getSettingItem(()=>this.onClick(MORE_MENU.Website),require('../my/img/ic_insert_emoticon.png'),MORE_MENU.Website,{tintColor:'#2196F3'})}
+   <View style={GlobalStyles.line} />
+   {ViewUtils.getSettingItem(()=>this.onClick(MORE_MENU.Feedback),require('../../../res/images/ic_feedback.png'),MORE_MENU.Feedback,{tintColor:'#2196F3'})}
+   <View style={GlobalStyles.line} />
+    </View>
+    return this.renderView(content,{
+      name: "GitHub Popular",
+      description:
+        "这是一个滴滴答答滴滴答答滴滴答答的描述这是一个滴滴答答滴滴答答滴滴答答的描述",
+      avatar: "https://avatar.csdn.net/6/7/C/1_qq_38366777.jpg?1536042584",
+      backgroundImg:
+        "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1333646673,365823664&fm=26&gp=0.jpg"
+    });
   }
 }
 
@@ -136,8 +151,9 @@ const styles = StyleSheet.create({
   },
   stickySection: {
     height: STICKY_HEADER_HEIGHT,
-    width: 300,
-    justifyContent: "flex-end"
+    justifyContent: "center",
+    alignItems: 'center',
+    paddingTop: Platform.OS === "ios" ? 20 : 0
   },
   stickySectionText: {
     color: "white",
@@ -146,8 +162,15 @@ const styles = StyleSheet.create({
   },
   fixedSection: {
     position: "absolute",
-    bottom: 10,
-    right: 10
+    bottom: 0,
+    right:10,
+    left: 0,
+    top:0,
+    paddingRight: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent:'space-between',
+    paddingTop: Platform.OS === "ios" ? 20 : 0
   },
   fixedSectionText: {
     color: "#999",
